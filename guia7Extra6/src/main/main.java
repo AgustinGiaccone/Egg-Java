26b3f124efc43561e37a642249b69eb2291c5db3
Agustin Giaccone <agustintgiaccone1999@gmail.com>
2023-04-19 16:02:47 -0300

    ejercicio extra 6 guia 7

---
+++ guia7Extra6/src/main/main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+import entidad.Rectangulo;
+
+public class main {
+
+    public static void main(String[] args) {
+        Rectangulo rectangulo1 = new Rectangulo(4,6);
+        System.out.println("el area del rectangulo1 es: "+ rectangulo1.calcular_area());
+    }
+    
+}
