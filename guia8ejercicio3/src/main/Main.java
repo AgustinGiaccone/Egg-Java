db9177b6ad28d356a7bad46729ee05f88a0fe8a8
Agustin Giaccone <agustintgiaccone1999@gmail.com>
2023-04-25 23:42:14 -0300

    ejercicio-3-INCOMPLETO

--- guia8ejercicio3/src/main/Main.java
+++ guia8ejercicio3/src/main/Main.java
@@ -9,101 +9,116 @@ public class Main {
  
     public static void main(String[] args) {
          PersonaServicio ps = new PersonaServicio();
-         int mayores = 0;
-         int menores=0;
-         int sobrepeso=0;
-         int ideal=0;
-         int delgado =0;
-         
-         
-         Persona p1 = ps.crearPersona();
-         System.out.println("es mayor de edad?? "+ ps.esMayorEdad(p1));
-         
-         if (ps.esMayorEdad(p1)==true) {
-            mayores ++;
-        }else{
-             menores ++;
-         }
-         
-         ps.calcularIMC(p1);
-         if(ps.calcularIMC(p1) ==-1){
-        delgado ++;
-    }else if (ps.calcularIMC(p1)==0){
-        ideal++;
-    }else if(ps.calcularIMC(p1)==1){
-        sobrepeso++;
-    }
-         
-         
-         
-         
-         Persona p2 = ps.crearPersona();
-         System.out.println("es mayor de edad?? "+ ps.esMayorEdad(p2));
-         
-         if (ps.esMayorEdad(p1)==true) {
-            mayores ++;
-        }else{
-             menores ++;
-         }
-         
-         ps.calcularIMC(p2);
-         if(ps.calcularIMC(p2) ==-1){
-        delgado ++;
-    }else if (ps.calcularIMC(p2)==0){
-        ideal++;
-    }else if(ps.calcularIMC(p2)==1){
-        sobrepeso++;
-    }
-         
-         
-         
-         
-         Persona p3 = ps.crearPersona();
-         System.out.println("es mayor de edad?? "+ ps.esMayorEdad(p3));
-         
-         if (ps.esMayorEdad(p1)==true) {
-            mayores ++;
-        }else{
-             menores ++;
-         }
-         
-         ps.calcularIMC(p3);
-         if(ps.calcularIMC(p3) ==-1){
-        delgado ++;
-    }else if (ps.calcularIMC(p3)==0){
-        ideal++;
-    }else if(ps.calcularIMC(p3)==1){
-        sobrepeso++;
-    }
-         
-         
-         
-         
-         Persona p4 = ps.crearPersona();
-         System.out.println("es mayor de edad?? "+ ps.esMayorEdad(p4));
-         
-         if (ps.esMayorEdad(p1)==true) {
-            mayores ++;
-        }else{
-             menores ++;
-         }
-         
-         ps.calcularIMC(p4);
-         if(ps.calcularIMC(p4) ==-1){
-        delgado ++;
-    }else if (ps.calcularIMC(p4)==0){
-        ideal++;
-    }else if(ps.calcularIMC(p4)==1){
-        sobrepeso++;
-    }
-         
-         
-        System.out.println("el porcentaje de personas con sobre peso es "+ sobrepeso/4*100) ; 
-        System.out.println("el porcentaje de personas en su peso ideal es  "+ ideal/4*100) ;
-        System.out.println("el porcentaje de personas delgadas es  "+ delgado/4*100) ;
+          double mayores=0;
+        double menores=0;
+        double sobrepeso=0;
+        double ideal=0;
+        double delgado=0;
         
-        System.out.println("el porcentajes de menores es " + menores/4*100);
-        System.out.println("el porcentajes de mayores es " + mayores/4*100); 
-    }
-    
+        System.out.println("");
+        System.out.println("PERSONA 1");
+        
+        Persona p1 = ps.crearPersona();
+        System.out.println("¿Mayor de edad?: "+ ps.esMayorEdad(p1));
+        
+        if(ps.esMayorEdad(p1) == true){
+            mayores++;
+        }else{
+            menores++;
+        }
+        
+        ps.mostratIMC(p1);
+                
+        if(ps.calcularIMC(p1) == -1){
+            delgado++;
+        }else if(ps.calcularIMC(p1) == 0){
+            ideal++;
+        }else if(ps.calcularIMC(p1) == 1){
+            sobrepeso++;
+        }
+        
+        
+    ////////////////////////////////////////////////////
+        System.out.println("");
+        System.out.println("PERSONA 2");
+        
+        Persona p2 = ps.crearPersona();
+        System.out.println("¿Mayor de edad?: "+ ps.esMayorEdad(p2));
+        
+        if(ps.esMayorEdad(p2) == true){
+            mayores++;
+        }else{
+            menores++;
+        }
+        
+        ps.mostratIMC(p2);
+        
+        if(ps.calcularIMC(p2) == -1){
+            delgado++;
+        }else if(ps.calcularIMC(p2) == 0){
+            ideal++;
+        }else if(ps.calcularIMC(p2) == 1){
+            sobrepeso++;
+        }
+        
+        
+    ////////////////////////////////////////////////////
+        System.out.println("");
+        System.out.println("PERSONA 3");
+        
+        Persona p3 = ps.crearPersona();
+       System.out.println("¿Mayor de edad?: "+ ps.esMayorEdad(p3));
+       
+       if(ps.esMayorEdad(p3) == true){
+            mayores++;
+        }else{
+            menores++;
+        }
+       //mostratIMC
+       //mostrarIMC
+       ps.mostratIMC(p3);
+        if(ps.calcularIMC(p3) == -1){
+            delgado++;
+        }else if(ps.calcularIMC(p3) == 0){
+            ideal++;
+        }else if(ps.calcularIMC(p3) == 1){
+            sobrepeso++;
+        }
+       
+    ////////////////////////////////////////////////////
+        System.out.println("");
+        System.out.println("PERSONA 4");
+        
+        Persona p4 = ps.crearPersona();
+        System.out.println("¿Mayor de edad?: "+ ps.esMayorEdad(p4));
+        
+        if(ps.esMayorEdad(p4) == true){
+            mayores++;
+        }else{
+            menores++;
+        }
+        
+        ps.mostratIMC(p4);
+        if(ps.calcularIMC(p4) == -1){
+            delgado++;
+        }else if(ps.calcularIMC(p4) == 0){
+            ideal++;
+        }else if(ps.calcularIMC(p4) == 1){
+            sobrepeso++;
+        }
+        
+        
+     /////////////////////////////////////////////////////////
+        System.out.println("==========================================");
+        System.out.println("El porcentaje de personas con sobrepeso es: " + (sobrepeso/4)*100 + "%");
+        System.out.println("El porcentaje de personas en su peso ideal es: " + (ideal/4)*100 + "%");
+        System.out.println("El porcentaje de personas debajo de su peso ideal es: " + (delgado/4)*100 + "%");
+        
+        System.out.println("==========================================");
+        
+        System.out.println("El porcentaje de menores de edad es: " +  (menores/4)*100 + "%");
+        System.out.println("El porcentaje de mayores de edad es: " +  (mayores/4)*100 + "%");
+        System.out.println("==========================================");
+        
+    } 
 }
